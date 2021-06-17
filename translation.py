af4ce30474adace00a1f88626ddc32fa8071ac1d
Mufaz TG <85679056+MufazTG@users.noreply.github.com>
2021-06-17 15:12:40 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,6 @@
 class Translation(object):
     START_MSG = """<b>Hello,
 
-I am a Simple Bot Forward The Files You Send to Me to your Channel</b>""" 
+I am a Simple Bot Forward The Files You Send to Me to your Channel</b>
+Maded By : @BX_Botz""" 
     HELP = """<code>Send me Any Medias to Forward to the Channel</code>"""  
